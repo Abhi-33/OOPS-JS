4bf0d05d10c977b395e2e250ef2d72406470d851
Abhishek Vashisth <bmeenakshi51@gmail.com>
2024-03-19 14:21:14 +0530

    Updated : Constructor script file

--- Constructor-Function/script.js
+++ Constructor-Function/script.js
@@ -3,6 +3,14 @@
 const Person = function(firstName,birthYear){
 this.firstName = firstName;
 this.birthYear = birthYear;
+
+// never do this - Never create an method inside a 
+// constructor function this is a bad practice
+// but instead to solve this problem you can use prototypes
+// and protypal inheritance
+// this.calcAge = function() {
+    // console.log(2024 - this.birthYear);
+// }
 }
 const Abhishek = new Person('Abhishek' , 2004);
 console.log(Abhishek);
@@ -12,3 +20,9 @@ console.log(Abhishek);
 //2. function is called , this={}
 //3. {} is linked to prototype
 //4. function automatically return {}. 
+const bhanu = new Person("Bhanu" , 2002);
+const kanika = new Person("Kanika" , 2001);
+console.log(bhanu , kanika);
+console.log(bhanu instanceof Person);
+//"instanceof" - An operator to check whether a variable is an instance of a 
+// constructor function(class) or not 
