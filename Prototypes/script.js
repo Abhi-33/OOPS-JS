3a9a68339f4a0c3ff2be63fdc600637b7297646d
Abhishek Vashisth <bmeenakshi51@gmail.com>
2024-03-20 15:18:00 +0530

    Updated : Prototypes  script file

--- Prototypes/script.js
+++ Prototypes/script.js
@@ -6,16 +6,34 @@ const Person = function(firstName,birthYear){
 }    
 const Abhishek  = new Person("Abhishek" , 2004);
 const Bhanu = new Person("Bhanu" , 2002);
-console.log(Person.prototype);
+// console.log(Person.prototype);
 //Prototypes
 Person.prototype.calcAge = function(){
-    console.log(2024 - this.birthYear);
+    // console.log(2024 - this.birthYear);
 }
 Abhishek.calcAge();
 Bhanu.calcAge();
-console.log(Abhishek.__proto__);
-console.log(Abhishek.__proto__ === Person.prototype);
-console.log(Person.prototype.isPrototypeOf(Abhishek));
+// console.log(Abhishek.__proto__);
+// console.log(Abhishek.__proto__ === Person.prototype);
+// console.log(Person.prototype.isPrototypeOf(Abhishek));
 
 Person.prototype.species = "Homo Sapiens";
-console.log(Abhishek , Bhanu);
+// console.log(Abhishek , Bhanu);
+
+console.log(Abhishek.__proto__);
+//Object.prototype is top of prototype chain
+console.log(Abhishek.__proto__.__proto__);
+console.log(Abhishek.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [1,2,3,5 ,5,3,6];
+console.log(arr.__proto__);
+
+//create a new method in the array prototype 
+Array.prototype.unique = function () {
+    return [...new Set(this)];
+}
+console.log(arr.unique());
+let Team = ['Abhi' , 'Abhi' , 'Bhanu' , 'Kanika' , 'Kanika'];
+console.log(Team.unique()); 
