9d49d4c81506f81cf4403c1137fe3a32bcb008f7
Abhishek Vashisth <bmeenakshi51@gmail.com>
2024-03-26 13:35:32 +0530

    Updated : getters and setters script

--- getters_and_setters/script.js
+++ getters_and_setters/script.js
@@ -1,70 +1,77 @@
- 'use strict';
- //simple example 
- const account = {
-    owner : "Abhi" , 
-    movements: [10,20,40,70],
+'use strict';
+//simple example 
+const account = {
+    owner: "Abhi",
+    movements: [10, 20, 40, 70],
 
-    get latest(){
+    get latest() {
         return this.movements.slice(-1).pop();
     },
-    set latest(mov){
+    set latest(mov) {
         return this.movements.push(mov); //this pushes the value of mov to the movements array.
     },
- };
- console.log(account.latest);
- account.latest = 100;
- console.log(account.movements);
+};
+console.log(account.latest);
+account.latest = 100;
+console.log(account.movements);
 
 //  getters and setters in Classes 
 class PersonCL {
-    constructor(firstName , birthYear){
-            this.firstName = firstName;
-            this.birthYear = birthYear;
-            console.log(`${this.firstName} Was born in  ${this.birthYear}`);
+    constructor(fullName, birthYear ) {
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+        console.log(`${this.firstName} Was born in  ${this.birthYear}`);
     }
-    calcAge(){
-        console.log( 2024 - this.birthYear );
+    calcAge() {
+        console.log(2024 - this.birthYear);
     }
-    greet(){
+    greet() {
         console.log(`Hey ! ${this.firstName} , How are you ?`);
     }
-    get age(){
+    get age() {
         return 2024 - this.birthYear;
     }
-    set age(value){
-         this.birthYear = value;
+    // set a property that already exists
+    set fullName(name){
+        console.log(name);
+        if(name.includes(' '))this._fullName = name;
+        else alert(`${name} is not a full name!`);
+    }
+
+    get fullName(){
+        return this._fullName;
     }
 }
 
-const Abhishek = new PersonCL("Abhishek" , 2004);
-console.log(Abhishek.age);
-Abhishek.age = 34;
-console.log(Abhishek.age); 
+const Abhishek = new PersonCL("Abhishek Vashisth", 2004);
+console.log(Abhishek);
+// Abhishek.age = 34;
 
+const Bhanu = new PersonCL("Bhanu" , 2003);
+console.log(Bhanu);
 
 // example practice of constructor making 
-class Car{
-    constructor(carName , carModel){
-            this.carName = carName;
-            this.carModel = carModel;
+class Car {
+    constructor(carName, carModel) {
+        this.carName = carName;
+        this.carModel = carModel;
     }
-    check(){
-        if(this.carModel <2012) {
+    check() {
+        if (this.carModel < 2012) {
             console.log('Not Available right Now')
         }
-        else if(this.carModel >2012){
+        else if (this.carModel > 2012) {
             console.log('Available ! You can come tomorrow');
         }
-        else if(this.carModel == 2012){
+        else if (this.carModel == 2012) {
             console.log('Car is Available but you have to pay a little extra to arrange spares for older model!');
         }
-        else{
+        else {
             console.log('come back later ');
         }
     }
 
 }
-const Swift = new Car('Swift' , 2012);
+const Swift = new Car('Swift', 2012);
 console.log(Swift);
 Swift.check();
-
