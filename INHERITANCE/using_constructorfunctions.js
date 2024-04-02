b783f9ed6412d9b77ca3a375c37268395df3eeb6
Abhishek Vashisth <bmeenakshi51@gmail.com>
2024-04-02 20:33:35 +0530

    Updated : Inheritance in Constructor functions

---
+++ INHERITANCE/using_constructorfunctions.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Person = function(firstName , birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+}
+
+Person.prototype.calcAge = function(){
+    console.log(2024 - this.birthYear);
+}
+
+const Student = function(firstName , birthYear , course) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+    this.course = course;
+}
+
+Student.prototype.introduce = function () {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);   
+}
