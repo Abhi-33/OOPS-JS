f28ddacdd448220d79c164c949d1674110a19612
Abhishek Vashisth <bmeenakshi51@gmail.com>
2024-04-02 20:35:35 +0530

    Updated : Inheritance in Constructor functions

--- INHERITANCE/using_constructorfunctions.js
+++ INHERITANCE/using_constructorfunctions.js
@@ -18,3 +18,6 @@ const Student = function(firstName , birthYear , course) {
 Student.prototype.introduce = function () {
     console.log(`My name is ${this.firstName} and I study ${this.course}`);   
 }
+
+const mike = new Student('Mike' , 2004 , 'Computer Science');
+mike.introduce();
